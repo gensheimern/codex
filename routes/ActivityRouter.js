0e3793f58e3193f5ec229a60865e267672c4bca5
Nico Gensheimer <nico-gensheimer@gmx.de>
2018-04-09 13:45:48 +0200

    added Activity and Message API

---
+++ routes/ActivityRouter.js
@@ -0,0 +1,64 @@
+var express = require('express');
+var router = express.Router();
+var Activity = require('../models/ActivityModel');
+
+router.get('/:id?', function(req, res, next) {
+
+  if (req.params.id) {
+    Activity.getActivityById(req.params.id, function(err, rows) {
+      if (err) {
+        res.json(err);
+      } else {
+        res.json(rows);
+      }
+    });
+  } else {
+    Activity.getAllActivitys(function(err, rows) {
+      if (err) {
+        res.json(err);
+      } else {
+        res.json(rows);
+      }
+
+    });
+  }
+});
+
+router.post('/', function(req, res, next) {
+
+  Activity.addActivity(req.body, function(err, count) {
+
+    console.log(req.body);
+
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(req.body); //or return count for 1 & 0
+    }
+  });
+});
+
+
+router.delete('/id', function(req, res, next) {
+
+  Activity.deleteActivity(req.params.id, function(err, count) {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(count);
+    }
+  });
+});
+
+router.put('/:id', function(req, res, next) {
+
+  Activity.updateActivity(req.params.id, req.body, function(err, rows) {
+
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(rows);
+    }
+  });
+});
+module.exports = router;
