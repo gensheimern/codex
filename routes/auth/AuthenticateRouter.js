e33b0cb05e47b0ee1381239a910883d2fb94a841
Nico Gensheimer <nico-gensheimer@gmx.de>
2018-04-12 15:22:26 +0200

    fix

--- routes/auth/AuthenticateRouter.js
+++ routes/auth/AuthenticateRouter.js
@@ -21,29 +21,30 @@ router.post('/', function(req, res, next) {
           "success": false,
           "Email": "not found"
         })
-      }
-
-      if (rows[0].Password != req.body.Password) {
-
-        res.sendStatus(403);
-
       } else {
 
-        var token = jwt.sign({
-            "Firstname": rows[0].Firstname,
-            "Name": rows[0].Name,
-            "Email": rows[0].Email,
-            iat: Math.floor(Date.now() / 1000) - 30,
-            exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24)
+        if (rows[0].Password != req.body.Password) {
 
-          },
-          'secret');
-        console.log(token);
-        res.json({
-          success: true,
+          res.sendStatus(403);
 
-          token: token
-        });
+        } else {
+
+          var token = jwt.sign({
+              "Firstname": rows[0].Firstname,
+              "Name": rows[0].Name,
+              "Email": rows[0].Email,
+              iat: Math.floor(Date.now() / 1000) - 30,
+              exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24)
+
+            },
+            'secret');
+          console.log(token);
+          res.json({
+            success: true,
+
+            token: token
+          });
+        }
       }
     }
   });
