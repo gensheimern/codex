e4544865a35e370d37d179c786fbba69933a19fe
Max Stockbauer <max.stockbauer1@web.de>
2018-04-13 22:07:03 +0200

    implemented delete buttons with referrence possibility TODO: delete api call to database

--- frontend/src/components/groupmanager/ListGroups.js
+++ frontend/src/components/groupmanager/ListGroups.js
@@ -1,11 +1,11 @@
 import React from "react";
 import {
-    Button,
-    FormGroup,
-    FormControl,
-    ControlLabel,
-    Alert,
-    Table
+  Button,
+  FormGroup,
+  FormControl,
+  ControlLabel,
+  Alert,
+  Table
 } from "react-bootstrap";
 import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 import "./groupmanager.css";
@@ -14,88 +14,124 @@ import config from '../../config';
 import BootstrapTable from 'react-bootstrap-table-next';
 
 export default class ListTeams extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            groups: []
-        };
-    }
-    nameHostId() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      groups: []
+    };
+  }
 
-        let resN;
-        for (let i = 0; i < this.state.groups.length; i++) {
-            let TMid = this.state.groups[i].Teammanager;
-            fetch(config.apiPath + "/user/" + TMid, {
-                headers: {
-                    'Content-Type': 'application/json',
-                    'X-Access-Token': localStorage.getItem('apiToken')
-                }
-            }).then((resN) => {
-                if (!resN.ok) {
-                    throw new Error("Request failed.");
-                } else if (resN.status !== 200) {
-                    throw new Error("Forbidden");
-                } else {
-                    return resN;
-                }
-            }).then(resN => resN.json()).then(resN => {
-                let resNM = this.state.groups;
+  SendTeamToDelete(Tid) {
 
-                if (resN.length > 0) 
-                    resNM[i].Teammanager = resN[0].Firstname + " " + resN[0].Name;
-                
-                this.setState({groups: resNM});
+    // TODO: senden an Server
+    // fetch(config.apiPath + "/Team/" + Tid, {
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //     'X-Access-Token': localStorage.getItem('apiToken')
+    //   }
+    // }).then((resN) => {
+    //   if (!resN.ok) {
+    //     throw new Error("Request failed.");
+    //   } else if (resN.status !== 200) {
+    //     throw new Error("Forbidden");
+    //   } else {
+    //     return resN;
+    //   }
+    // }).then(resN => resN.json()).then(resN => {});
+  }
 
-                // }).catch((err) => {
-                //     this.setState({groups: []});
-            });
+  nameHostId() {
+
+    let resN;
+    for (let i = 0; i < this.state.groups.length; i++) {
+      let TMid = this.state.groups[i].Teammanager;
+      let Tid = this.state.groups[i].Team_Id;
+      fetch(config.apiPath + "/user/" + TMid, {
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Access-Token': localStorage.getItem('apiToken')
         }
+      }).then((resN) => {
+        if (!resN.ok) {
+          throw new Error("Request failed.");
+        } else if (resN.status !== 200) {
+          throw new Error("Forbidden");
+        } else {
+          return resN;
+        } //SendTeamToDelete(Tid)
+      }).then(resN => resN.json()).then(resN => {
+        let resNM = this.state.groups;
+        let theButton //SendTeamToDelete(Tid)
+        theButton = <button type="button" onClick={() => console.log("ok")}>
+          Delete the Group
+        </button>
+        resNM[i].button = theButton;
+        if (resN.length > 0) 
+          resNM[i].Teammanager = resN[0].Firstname + " " + resN[0].Name;
+        
+        this.setState({groups: resNM});
 
-    }
-    componentDidMount() {
-        this.loadContent();
+        // }).catch((err) => {
+        //     this.setState({groups: []});
+      });
     }
 
-    loadContent() {
-        fetch(config.apiPath + "/team", {
-            headers: {
-                'Content-Type': 'application/json',
-                'X-Access-Token': localStorage.getItem('apiToken')
-            }
-        }).then((res) => {
-            if (!res.ok) {
-                throw new Error("Request failed.");
-            } else if (res.status !== 200) {
-                throw new Error("Forbidden");
-            } else {
-                return res;
-            }
-        }).then(res => res.json()).then(res => {
-            this.setState({groups: res});
-            this.nameHostId();
-        }).catch((err) => {
-            this.setState({groups: []});
-        });
+  }
+  componentDidMount() {
+    this.loadContent();
+  }
 
-    }
-    render() {
-        const columns = [
-            {
-                dataField: 'Team_Id',
-                text: 'Team ID'
-            }, {
-                dataField: 'Teamname',
-                text: 'Team Name'
-            }, {
-                dataField: 'Teammanager',
-                text: 'Team Leiter'
-            }
-        ];
+  loadContent() {
+    fetch(config.apiPath + "/team", {
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Access-Token': localStorage.getItem('apiToken')
+      }
+    }).then((res) => {
+      if (!res.ok) {
+        throw new Error("Request failed.");
+      } else if (res.status !== 200) {
+        throw new Error("Forbidden");
+      } else {
+        return res;
+      }
+    }).then(res => res.json()).then(res => {
+      this.setState({groups: res});
+      this.nameHostId();
+    }).catch((err) => {
+      this.setState({groups: []});
+    });
 
-        return (<div className="listTeams">
-            <div>
-                <BootstrapTable responsive="responsive" keyField='Team_Id' data={this.state.groups} columns={columns}/>
-            </div>
-        </div>);
-    }
+  }
+  render() {
+    const columns = [
+      {
+        style: {},
+        classes: 'span-1',
+        dataField: 'Team_Id',
+        text: 'Team ID'
+        // headerStyle: {
+        //   backgroundColor: 'lightblue'
+      }, {
+        classes: 'span-2',
+        dataField: 'Teamname',
+        text: 'Team Name'
+      }, {
+        classes: 'span-2',
+        dataField: 'Teammanager',
+        text: 'Team Leiter'
+      }, {
+        classes: 'span-1',
+        dataField: 'button',
+        text: 'Eintrag löschen',
+        allign: 'center'
+      }
+    ];
+
+    return (<div className="listTeams">
+      <div>
+        <BootstrapTable responsive="responsive" keyField='Team_Id' data={this.state.groups} columns={columns}/>
+      </div>
+    </div>);
+  }
 }
