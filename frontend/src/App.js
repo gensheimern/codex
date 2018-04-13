da7f45cd6900952533447b53451f89d6b0a712f5
Max Stockbauer <max.stockbauer1@web.de>
2018-04-13 11:56:05 +0200

    frontend groupmanagement 1/2 done -max

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,29 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import React, {Component} from 'react';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
 
 // Components
 import './App.css';
 import Login from './components/login/Login';
-import CreateTeam from './components/CreateGroup';
-import ListTeams from './components/ListGroups';
+import CreateTeam from './components/groupmanager/CreateGroup';
+import ListTeams from './components/groupmanager/ListGroups';
 import ListUsers from './components/ListUsers';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {};
-  }
+        this.state = {};
+    }
 
-  render() {
-    return (
-      <BrowserRouter>
-        <React.Fragment>
-          <Route exact path="/" component={Login} />
-          <Route exact path="/create_group" component={CreateTeam} />
-          <Route exact path="/create_group" component={ListTeams} />
-        </React.Fragment>
-      </BrowserRouter>
-    );
-  }
+    render() {
+        return (<BrowserRouter>
+            <React.Fragment>
+                <Route exact="true" path="/" component={Login}/>
+                <Route exact="true" path="/create_group" component={CreateTeam}/>
+                <Route exact="true" path="/create_group" component={ListTeams}/>
+            </React.Fragment>
+        </BrowserRouter>);
+    }
 }
 
 export default App;
