d3ae3b11bac4a85643619efc93f8551f27e7e41f
Max Stockbauer <max.stockbauer1@web.de>
2018-05-25 15:13:17 +0200

    Profile superbasic no userid get and styles
    -> need userid method asis hardcoded
    -> styles and picture missing

--- frontend/src/components/MainContent.js
+++ frontend/src/components/MainContent.js
@@ -3,93 +3,86 @@ import Events from './event/Events';
 import Personal from './MenuComponents/SidebarContentCalender';
 import CreateEvent from './activity/CreateActivity';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import ProfileContent from './Profile.js';
 
 export default class MainContent extends React.Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-		displayedContent:"1",
-		};
-	}
+    this.state = {
+      displayedContent: "1"
+    };
+  }
 
-	render() {
+  render() {
 
-		console.log(this.props.mainContentNumber);
+    console.log(this.props.mainContentNumber);
 
-		if(this.props.mainContentNumber === 0){
-		return (
-			<div style={{
-				width: this.props.width,
-				marginTop: "0%",
-				float: "left",
-				height: "100%",
-				overflowY: "scroll",
-			}}>
-				<Events />
-			</div>
-		);
-	}else if(this.props.mainContentNumber === 1){
-		return (
-			<div style={{
-				width: this.props.width,
-				marginTop:"0%",
-				margin:"5%",
-				float: "left",
-				height: "100%",
-				overflowY: "scroll",
-			}}>
-				<Personal />
-			</div>
-		);
-	}else	if(this.props.mainContentNumber === 2){
-		return (
-			<div style={{
-				width: this.props.width,
-				marginTop: "0%",
-				float: "left",
-				height: "100%",
-				overflowY: "scroll",
-				margin:"10%"
-			}}>
-			<MuiThemeProvider>
-				<CreateEvent/>
-			</MuiThemeProvider>
-			</div>);
-	}else	if(this.props.mainContentNumber === 3){
-		return (
-			<div style={{
-				width: this.props.width,
-				marginTop: "0%",
-				float: "left",
-				height: "100%",
-				overflowY: "scroll",
-			}}>
-				<Events />
-			</div>);
-	}else	if(this.props.mainContentNumber === 4){
-			return (
-				<div style={{
-					width: this.props.width,
-					marginTop: "0%",
-					float: "left",
-					height: "100%",
-					overflowY: "scroll",
-				}}>
-					<Events />
-				</div>);
-		}else	if(this.props.mainContentNumber === 5){
-				return (
-					<div style={{
-						width: this.props.width,
-						marginTop: "0%",
-						float: "left",
-						height: "100%",
-						overflowY: "scroll",
-					}}>
-						<Events />
-					</div>);
-			}
+    if (this.props.mainContentNumber === 0) {
+      return (<div style={{
+          width: this.props.width,
+          marginTop: "0%",
+          float: "left",
+          height: "100%",
+          overflowY: "scroll"
+        }}>
+        <Events/>
+      </div>);
+    } else if (this.props.mainContentNumber === 1) {
+      return (<div style={{
+          width: this.props.width,
+          marginTop: "0%",
+          margin: "5%",
+          float: "left",
+          height: "100%",
+          overflowY: "scroll"
+        }}>
+        <Personal/>
+      </div>);
+    } else if (this.props.mainContentNumber === 2) {
+      return (<div style={{
+          width: this.props.width,
+          marginTop: "0%",
+          float: "left",
+          height: "100%",
+          overflowY: "scroll",
+          margin: "10%"
+        }}>
+        <MuiThemeProvider>
+          <CreateEvent/>
+        </MuiThemeProvider>
+      </div>);
+    } else if (this.props.mainContentNumber === 3) {
+      return (<div style={{
+          width: this.props.width,
+          marginTop: "0%",
+          float: "left",
+          height: "100%",
+          overflowY: "scroll"
+        }}>
+        <Events/>
+      </div>);
+    } else if (this.props.mainContentNumber === 4) {
+      return (<div style={{
+          width: this.props.width,
+          marginTop: "0%",
+          float: "left",
+          height: "100%",
+          overflowY: "scroll"
+        }}>
+        <ProfileContent/>
+      </div>);
+    } else if (this.props.mainContentNumber === 5) {
+      return (<div style={{
+          width: this.props.width,
+          marginTop: "0%",
+          float: "left",
+          height: "100%",
+          overflowY: "scroll"
+        }}>
+        <Events/>
+      </div>);
+    }
 
-	}
+  }
 }
---
+++ frontend/src/components/Profile.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import config from '../config';
+
+export default class ProfileContent extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      user: []
+    };
+    this.getMyProfile = this.getMyProfile.bind(this);
+    this.componentDidMount = this.componentDidMount.bind(this);
+  }
+
+  getMyProfile() {
+    fetch(config.apiPath + "/user/666", {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Access-Token': localStorage.getItem('apiToken')
+      }
+    }).then((res) => {
+      console.log(res.status);
+      if (!res.ok) {
+        throw new Error("Request failed.");
+      } else if (res.status !== 200) {
+        throw new Error("Forbidden");
+      } else {
+        return res;
+      }
+    }).then(res => res.json()).then(res => {
+      console.log(res);
+      this.setState({user: res});
+    });
+
+  }
+  componentDidMount() {
+    this.getMyProfile();
+  }
+
+  render() {
+    console.log(this.state.user);
+
+    return (<div className="Profile" style={{
+        marginTop: "10%"
+      }}>
+      <p>
+        || User || ____________________________________________________________________________________________________________________
+      </p>
+      <p>
+        {this.state.user.firstName}</p>
+      <p>
+        {this.state.user.name}</p>
+      <p>
+        {this.state.user.email}</p>
+      <p>
+        {this.state.user.image}</p>
+    </div>);
+  }
+}
