4368c784b07445e16935b7acd1ca3f66c24defe4
ModuloGames <max-granzow@t-online.de>
2018-05-28 11:55:04 +0200

    Removed old tests.

--- frontend/src/components/login/Login.komp.test.js
+++ frontend/src/components/login/Login.komp.test.js
@@ -15,24 +15,25 @@ describe('Login Component', () =>{
 
     it('renders a password input', () => {
         expect(shallow(<Login />).find('#password').length).toEqual(1)
-       });
+    });
 
 
-       describe('Email input', () => {
-           it('should respond to change event and change the state of the Login Component', () =>{
-               const wrapper = shallow(<Login />)
-               wrapper.find('#email').simulate('change', {target:
-            {name: 'email', value: 'blah@gmail.com'}})
-           })
-       })
-       
-       describe('Password input', () => {
-  
-        it('should respond to change event and change the state of the Login Component', () => {
-         const wrapper = shallow(<Login />)
-         wrapper.find('#password').simulate('change', {target: {name: 'password', value: 'cats'}})
-         expect(wrapper.state('password')).toEqual('cats')
+    /* describe('Email input', () => {
+        it('should respond to change event and change the state of the Login Component', () =>{
+            const wrapper = shallow(<Login />)
+            wrapper.find('#email').simulate('change', {target:
+                {name: 'email', value: 'blah@gmail.com'}
+            });
+            expect(wrapper.state('password')).toEqual('blah@gmail.com')
         })
-       })
+    })
+       
+    describe('Password input', () => {
+        it('should respond to change event and change the state of the Login Component', () => {
+            const wrapper = shallow(<Login />)
+            wrapper.find('#password').simulate('change', {target: {name: 'password', value: 'cats'}})
+            expect(wrapper.state('password')).toEqual('cats')
+        })
+    }) */
      
 });
