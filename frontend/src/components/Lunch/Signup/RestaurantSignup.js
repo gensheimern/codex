793ff4363f28626fc04023638d556bace86d42fb
Nico Gensheimer <nico-gensheimer@gmx.de>
2018-06-11 11:31:42 +0200

    lkjslkj

--- frontend/src/components/Lunch/Signup/RestaurantSignup.js
+++ frontend/src/components/Lunch/Signup/RestaurantSignup.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReCAPTCHA from 'react-google-recaptcha';
+// import ReCAPTCHA from 'react-google-recaptcha';
 import config from '../../../config';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
