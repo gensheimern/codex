256b1e787cf5474259dacb76c0d2dd49380bc10d
ModuloGames <max-granzow@t-online.de>
2018-06-12 14:22:12 +0200

    Fixed bug in create event

--- models/ActivityModel.js
+++ models/ActivityModel.js
@@ -85,7 +85,11 @@ const Activity = {
 		const eventTag = activity.event ? 1 : 0;
 		const privateTag = activity.private ? 1 : 0;
 
-		return databaseConnection.queryp('INSERT INTO Activity (Description, Activityname, Place, Time, Eventtag, Private, Host, Banner, MaxParticipants, Organization) VALUES (?,?,?,?,?,?,?,?,?)', [activity.description, activity.name, activity.place, activity.time, eventTag, privateTag, userId, activity.banner, activity.maxParticipants, organizationId]);
+		if (organizationId === null) {
+			return databaseConnection.queryp('INSERT INTO Activity (Description, Activityname, Place, Time, Eventtag, Private, Host, Banner, MaxParticipants, Organization) VALUES (?,?,?,?,?,?,?,?, ?, NULL)', [activity.description, activity.name, activity.place, activity.time, eventTag, privateTag, userId, activity.banner, activity.maxParticipants]);
+		}
+
+		return databaseConnection.queryp('INSERT INTO Activity (Description, Activityname, Place, Time, Eventtag, Private, Host, Banner, MaxParticipants, Organization) VALUES (?,?,?,?,?,?,?,?,?,?)', [activity.description, activity.name, activity.place, activity.time, eventTag, privateTag, userId, activity.banner, activity.maxParticipants, organizationId]);
 	},
 
 	/**
