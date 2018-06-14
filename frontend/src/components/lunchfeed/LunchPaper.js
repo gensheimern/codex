6ccc9b90313d30cca034ac0c6f27c22c9e4be2ce
unknown <nico-gensheimer@gmx.de>
2018-06-14 13:24:24 +0200

    upsidaysie

--- frontend/src/components/lunchfeed/LunchPaper.js
+++ frontend/src/components/lunchfeed/LunchPaper.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Paper from 'material-ui/Paper';
 import './LunchFeed.css';
+import RaisedButton from 'material-ui/RaisedButton';
+import Dialog from 'material-ui/Dialog';
 
 const style = {
   height: "300px",
@@ -15,24 +17,69 @@ class LunchPaper extends React.Component {
 constructor(props){
   super(props);
   this.state = {
-
+    open: false,
   }
 }
+
+handleOpen = () => {
+  this.setState({open: true});
+};
+
+handleClose = () => {
+  this.setState({open: false});
+};
+
+checkForImage = () => {
+  console.log(this.props.image);
+  if(this.props.image === null){
+    return ("weblogin.jpg")
+  }else {
+    return ("lunch_"+this.props.image)
+  }
+}
+
+checkForText = () => {
+  console.log(this.props.text);
+  if(this.props.text === ""){
+    return (
+      <div>
+      this.props.name + " uploaded his lunch as a picture. Please Click here to see more info"
+      <RaisedButton onClick={this.handleOpen} label="LUNCHCARD" secondary={true} style={{marginTop:"30px",}} />
+      </div>
+    )
+  }else {
+    return (this.props.text)
+  }
+}
+
 
 render() {
   return(
   <div>
-    <Paper style={style} zDepth={3} >
+  <Dialog
+    modal={false}
+    open={this.state.open}
+    onRequestClose={this.handleClose}
+  >
+  <img src={"lunch_" + this.props.image} alt="LunchImageDialog" className="LunchImageDialog"/>
+  </Dialog>
+   <Paper style={style} zDepth={3} >
     <div className="LunchImageContainer">
-      <img src="weblogin.jpg" alt="LunchImage" className="LunchImage"/>
+      <img src={this.checkForImage()} alt="LunchImage" className="LunchImage"/>
       <div className="LunchImageMiddle">
         <div className="LunchImageText">
-          <h1> jojojo </h1>
+          <h4> {this.props.name} </h4>
         </div>
       </div>
-
     </div>
-
+    <div className="LunchTextBody">
+    <div className="LunchAddress">
+      <h3 stlye={{color:"#1ea185"}}>{this.props.place + " " + this.props.street + " " + this.props.streetNumber}</h3>
+    </div>
+      <div className="text">
+       {this.checkForText()}
+      </div>
+    </div>
     </Paper>
   </div>
 );
