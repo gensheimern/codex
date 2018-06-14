1432f7ea0db73d1155c0f4fd6b5374bc0c80ea91
ModuloGames <max-granzow@t-online.de>
2018-06-14 10:29:43 +0200

    Improved code style

--- frontend/src/components/event/CollapsedContent.js
+++ frontend/src/components/event/CollapsedContent.js
@@ -9,219 +9,203 @@ import FlatButton from 'material-ui/FlatButton';
 
 export default class CollapsedContent extends React.Component {
 
-  constructor(props){
-    super(props);
+	constructor(props){
+		super(props);
 
-    this.state = {
-      imgPath: '',
-      value:'',
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this._onKeyPress = this._onKeyPress.bind(this);
-  }
+		this.state = {
+			imgPath: '',
+			value: '',
+			comment: '',
+		};
 
-  _onKeyPress(event) {
-  if (event.charCode === 13) { // enter key pressed
-    event.preventDefault();
+		this.handleChange = this.handleChange.bind(this);
+		this._onKeyPress = this._onKeyPress.bind(this);
+	}
 
-    fetch(config.apiPath + "/activity/" + this.props.event.id + "/message", {
-      method: 'POST',
-      body: JSON.stringify({
-        content: this.refs.myTextfield.getValue()
+	_onKeyPress(event) {
+		if (event.key === 'Enter') { // enter key pressed
+			event.preventDefault();
 
-              }),
+			fetch(`${config.apiPath}/activity/${this.props.event.id}/message`, {
+				method: 'POST',
+				body: JSON.stringify({
+					content: this.state.value,
+				}),
+				headers: {
+					'Content-Type': 'application/json',
+					'X-Access-Token': localStorage.getItem('apiToken'),
+				},
+			})
+			.then((res) => {
+				if (!res.ok) {
+					throw new Error("Request failed.");
+				} else if (res.status !== 201) {
+					throw new Error("Forbidden");
+				}
 
-      headers: {
-        'Content-Type': 'application/json',
-        'X-Access-Token': localStorage.getItem('apiToken')
-      }
-    })
-    .then((res) => {
-      if (!res.ok) {
-        throw new Error("Request failed.");
-      } else if (res.status !== 201) {
-        throw new Error("Forbidden");
-      }
+				return res.json();
+			})
+			.then(res => {
+				this.props.loadMessages();
+				this.setState({value: ''});
+			})
+			.catch((err) => {
+				this.setState({
+					error: 'An Error occured.',
+				});
+			});
+		}
+	}
 
-      return res;
-    })
-    .then(res => res.json())
-    .then(res => {
-      this.props.loadMessages();
-      this.setState({value:''});
-    })
+	componentDidMount() {
+		fetch(`${config.apiPath}/user/me`, {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+				'X-Access-Token': localStorage.getItem('apiToken')
+			},
+		})
+		.then((res) => {
+			if (!res.ok) {
+				throw new Error("Request failed.");
+			} else if (res.status !== 200) {
+				throw new Error("Forbidden");
+			}
 
-    .catch((err) => {
-      console.log(err);
+			return res.json();
+		})
+		.then(res => {
+			this.setState({
+				imgPath: res.image
+			});
+		})
+		.catch((err) => {
+			// TODO
+		});
+	}
 
-      this.setState({
-        error: 'An Error occured.',
-      });
-    });
+	handleChange(e){
+		this.setState({
+			value: e.target.value,
+		});
+	}
 
+	deleteComment = (eventId, messageId) => () => {
+		fetch(`${config.apiPath}/activity/${eventId}/message/${messageId}`, {
+			method: 'DELETE',
+			headers: {
+				'Content-Type': 'application/json',
+				'X-Access-Token': localStorage.getItem('apiToken'),
+			}
+		})
+		.then((res) => {
+			this.props.loadMessages();
+			// TODO: Error handling
+		})
+	}
 
-  }
+	ToggleCollapse(){
+		if(this.props.collapse) {
+			let participatesIMG;
+			let participatesIMGPlus;
+			let message;
 
+			if (this.props.messages.length !==0 ) {
+				message = this.props.messages.map((messageItem, index) => {
+					return (
+						<div className="commentWrapper" key={"messageItem"+index}>
+							<div className="commentUserImage">
+								<img src={messageItem.author.image} alt="" />
+							</div>
+							<div className="commentInfoWrapper">
+								<div className="commentContentWrapper">
+									<span id="commentName"> {messageItem.author.name + " " + messageItem.author.firstName} </span>
+									<h6>{messageItem.content}</h6>
+									<h6>{dateParser.DateparserTime(messageItem.time)}</h6>
+								</div>
+							</div>
+							<div className="commentDelete">
+								{messageItem.author.me
+									? <FlatButton
+										icon={<DeleteMUI/>}
+										onClick={this.deleteComment(this.props.event.id, messageItem.id)}
+									/>
+									: <span />
+								}
+							</div>
+						</div>
+					)
+				});
+			}
 
-}
+			if (this.props.participants.length !== 0) {
+				//Mapping trough the participates array and returning the profile picture
+				participatesIMG = this.props.participants.map((participatesItem, index) =>
+					index <= 3 ? (
+						<img
+							className="myimage"
+							key={index}
+							src={ participatesItem.image }
+							alt="profile"
+						/>
+					) : true);
+			}
 
-  componentDidMount() {
-    fetch(config.apiPath + "/user/me", {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'X-Access-Token': localStorage.getItem('apiToken')
-      },
-    })
-    .then((res) => {
-      if (!res.ok) {
-        throw new Error("Request failed.");
-      } else if (res.status !== 200) {
-        throw new Error("Forbidden");
-      }
-
-      return res;
-    })
-    .then(res => res.json())
-    .then(res => {
-      this.setState({
-        imgPath: res.image
-      });
-    })
-    .catch((err) => {
-      // TODO
-    });
-  }
-
-  handleChange(e){
-    this.setState({ value: e.target.value });
-  }
-
-  ToggleCollapse(){
-
-    if(this.props.collapse){
-      let participatesIMG;
-      let participatesIMGPlus;
-      let message;
-      let counter = 1;
-      let eventRef = this.props.event.id;
-
-      if (this.props.messages.length !==0 ){
-        message = this.props.messages.map((messageItem, index) => {
-          return (
-            <div className="commentWrapper" key={"messageItem"+index}>
-              <div className="commentUserImage">
-                  <img src={messageItem.author.image} alt="" />
-              </div>
-              <div className="commentInfoWrapper">
-                <div className="commentContentWrapper">
-                    <span id="commentName"> {messageItem.author.name + " " + messageItem.author.firstName} </span>
-                    <h6>{messageItem.content}</h6>
-                    <h6>{dateParser.DateparserTime(messageItem.time)}</h6>
-                </div>
-              </div>
-              <div className="commentDelete">
-              {messageItem.author.me ? <FlatButton
-                                        icon={<DeleteMUI/>}
-                                        onClick={() => { return fetch(config.apiPath + "/activity/" + eventRef+
-                                                                      "/message/" + messageItem.id, {
-                                    			method: 'DELETE',
-                                    			headers: {
-                                    				'Content-Type': 'application/json',
-                                    				'X-Access-Token': localStorage.getItem('apiToken'),
-                                    			}
-                                    		})
-                                    		.then((res) => {
-                                          this.props.loadMessages()
-
-                                    			if (!res.ok || res.status !== 201) {
-                                    				// handle error
-                                    			} else {
-                                    				// this.renderSnackbar();
-                                    				// this.props.history.push('/feed');
-                                    			}
-                                    		})
-                                    	}}/>
-                                       : <span />}
-
-              </div>
-            </div>
-          )
-        });
-      }
-
-      if (this.props.participants.length !== 0) {
-        //Mapping trough the participates array and returning the profile picture
-          participatesIMG = this.props.participants.map((participatesItem, index) => {
-              if(counter<=4){
-                counter = counter+1;
-                return ( <
-                  img className = "myimage" key={index}
-                  src = {
-                      participatesItem.image
-                  }
-                  alt = "profile" / >
-              );
-            }
-          return true;});
-      }
-
-      if (this.props.participants.length >4) {
-        participatesIMGPlus = <div className="participants-counter-icon"><h6> +{this.props.participants.length -4}</h6></div>
-      }
+			if (this.props.participants.length > 4) {
+				participatesIMGPlus = <div className="participants-counter-icon"><h4> +{this.props.participants.length - 4}</h4></div>
+			}
 
 
-      return (
-        <div className="collapse-activity">
-          <div className="event-extend-info">
-              <h4> <PlaceMUI />  {this.props.event.place} </h4>
-          </div>
-            <div className="extendedInfo">
-                <div className="alreadyJoining">
-                    <h6> Already joining </h6>
-                </div>
-                <div className="joinInfo">
-                  <div className="participants-images">
-                    <div className="participants-row">{participatesIMG}</div> {participatesIMGPlus}
-                      <span id="participant-counter"> <h6><GroupFA />{" "} {this.props.participants.length}/{this.props.event.maxParticipants} </h6></span>
-                  </div>
+			return (
+				<div className="collapse-activity">
+					<div className="event-extend-info">
+						<h4> <PlaceMUI />  {this.props.event.place} </h4>
+					</div>
+					<div className="extendedInfo">
+						<div className="alreadyJoining">
+							<h6> Already joining </h6>
+						</div>
+						<div className="joinInfo">
+							<div className="participants-images">
+								<div className="participants-row">{participatesIMG}</div> {participatesIMGPlus}
+									<span id="participant-counter"> <h6><GroupFA />{" "} {this.props.participants.length}/{this.props.event.maxParticipants} </h6></span>
+								</div>
+							</div>
+						</div>
+						<div style={{ clear: 'both' }} />
+						<hr className="activity-hr" />
+						<div className="event-textfield">
+							{message}
+							<div className="texfield-profile-picture">
+								<img src= {this.state.imgPath} alt=""/>
+							</div>
+							<div  className="myTextfield">
+							<TextField
+								value={this.state.value}
+								hintText= "Add a new comment"
+								fullWidth={true}
+								className="addComment"
+								onKeyPress={this._onKeyPress}
+								onChange={this.handleChange}
+							/>
+						</div>
+					</div>
+				</div>
+			);
+		} else {
+			return(
+			<div className="collapse-activity">
+					<hr className="activity-hr" />
+			</div>
+		);
+		}
+	}
 
-                </div>
-            </div>
-            <div style={{clear:"both"}}> </div>
-            <hr className="activity-hr" />
-            <div className="event-textfield">
-                {message}
-                <div className="texfield-profile-picture">
-                    <img src= {this.state.imgPath} alt=""/>
-                </div>
-                <div  className="myTextfield">
-                <TextField
-                      value={this.state.value}
-                      hintText= "Add a new comment"
-                      fullWidth={true}
-                      className="addComment"
-                      onKeyPress={this._onKeyPress}
-                      onChange={this.handleChange}
-                      ref="myTextfield"
-                />
-                </div>
-            </div>
-        </div>
-      );
-    } else {
-      return(
-      <div className="collapse-activity">
-          <hr className="activity-hr" />
-      </div>
-    );
-    }
-  }
-
-render (){
-  return (<div>
-      {this.ToggleCollapse()}
-      </div>
-  )
-}
+	render () {
+		return (
+			<div>
+				{this.ToggleCollapse()}
+			</div>
+		)
+	}
 }
--- frontend/src/components/event/CreateEventChip.js
+++ frontend/src/components/event/CreateEventChip.js
@@ -2,47 +2,31 @@ import React from 'react';
 import Avatar from 'material-ui/Avatar';
 import Chip from 'material-ui/Chip';
 
-
 const styles = {
-  chip: {
-    margin: 4,
-  },
-  wrapper: {
-    display: 'flex',
-    flexWrap: 'wrap',
-  },
+	wrapper: {
+		display: 'flex',
+		flexWrap: 'wrap',
+	},
 };
 
-function handleRequestDelete() {
-  alert('You clicked the delete button.');
-}
-
 function handleClick() {
-  alert('You clicked the Chip.');
+	// TODO: Show info about user
 }
 
-/**
- * Examples of Chips, using an image [Avatar](/#/components/font-icon), [Font Icon](/#/components/font-icon) Avatar,
- * [SVG Icon](/#/components/svg-icon) Avatar, "Letter" (string) Avatar, and with custom colors.
- *
- * Chips with the `onRequestDelete` property defined will display a delete icon.
- */
-export default class ChipExampleSimple extends React.Component {
+export default class CreateEventChip extends React.Component {
 
-  render() {
-    return (
-      <div style={styles.wrapper}>
-
-        <Chip
-          onRequestDelete={handleRequestDelete}
-          onClick={handleClick}
-          style={styles.chip}
-        >
-          <Avatar src={this.props.peopleImage} />
-         {this.props.name}
-        </Chip>
-
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div style={styles.wrapper}>
+				<Chip
+					onRequestDelete={this.props.delete}
+					onClick={handleClick}
+					style={{ margin: 4 }}
+				>
+					<Avatar src={this.props.peopleImage} />
+					{this.props.name}
+				</Chip>
+			</div>
+		);
+	}
 }
